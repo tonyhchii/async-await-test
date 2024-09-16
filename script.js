e30744317b169551734a550e7f2d5e743985eb67
Tony Huang <170560060+tonyhchii@users.noreply.github.com>
2024-09-16 15:09:14 -0400

    added error message when no image is found

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ const getImage = () => {
   const searchInput = document.getElementById("gif-search");
   const searchString = searchInput.value;
   const searchUrl = `https://api.giphy.com/v1/gifs/translate?api_key=UjwVpv5mjdobyU83oKn8dVCDGYxbWRJn&s=${searchString}`;
-  fetch(searchUrl)
+  try {
+    fetch(searchUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      img.setAttribute("src", response.data.images.original.url);
+    
+      if (response.data.images) {
+        img.src = response.data.images.original.url;
+      }
+      else {
+        console.error('no image found');
+      }
     })
-    .catch((err) => {
-      console.error(err);
-    });
+  }
+  catch (error) {
+    console.error(error);
+  }
 };
 
 const imgBtn = document.querySelector(".img-btn");
